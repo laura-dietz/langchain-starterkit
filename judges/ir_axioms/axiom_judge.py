249de9f7d48bd3b0a32ab6fed4660eabda5821be
Maik Fröbe <maik.froebe@uni-jena.de>
2026-06-07 13:02:18 +0200

    mf

--- judges/ir_axioms/axiom_judge.py
+++ judges/ir_axioms/axiom_judge.py
@@ -156,7 +156,7 @@ class IrAxiomJudge(AutoJudge):
                 )
 
         leaderboard: Leaderboard = builder.build()
-        LeaderboardVerification(leaderboard, on_missing="ignore").all()
+        LeaderboardVerification(leaderboard, on_missing="fix_aggregate", warn=True).all()
         return leaderboard
 
 
